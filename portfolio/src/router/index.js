8ef60b45f35fade021d271123a08b2601e770683
tg8685 <tg8685@gmail.com>
2022-05-27 11:25:54 +0900

    feat: update deploy

--- portfolio/src/router/index.js
+++ portfolio/src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
   {
-    path: "/",
+    path: "/portfolio",
     name: "Main",
     component: () => import("../views/Main.vue"),
   },
